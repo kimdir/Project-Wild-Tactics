267117ec946429fad7f206b44be40d5622e5bcea
Michael <m.stebbins1120@gmail.com>
2022-03-21 15:51:09 -0500

    Initial Pt1

---
+++ Project Wild Tactics/Assets/TouchPhaseDisplay.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchPhaseDisplay : MonoBehaviour
+{
+    public Text phaseDisplayTest;
+    private Touch inputTouch;
+    private float timeTouchEnded;
+    private float displayTime = 0.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            inputTouch = Input.GetTouch(0);
+
+            if (inputTouch.phase == TouchPhase.Ended)
+            {
+                phaseDisplayText.text = inputTouch.phase.ToString();
+                timeTouchEnded = Time.time;
+            }
+
+            else if (Time.time - timeTouchEnded > displayTime)
+            {
+                phaseDisplayText.text = inputTouch.phase.ToString();
+                timeTouchEnded = Time.time;
+            }
+        }
+
+        else if (Time.time - timeTouchEnded > displayTime)
+        {
+            phaseDisplayText.text = "";
+        }
+    }
+}
